4fdd3340871eda8d8865bf4fb87cc37c3f9788ed
bear0473 <bear04731@gmail.com>
2017-01-01 14:57:33 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,13 +16,17 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-
- def create
+  def create
     @group = Group.new(group_params)
+    @group.save
 
-    if @group.save
       redirect_to groups_path
-    else
-      render :new
-    end
   end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+end
